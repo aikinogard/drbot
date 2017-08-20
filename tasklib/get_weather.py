82fab81a738f1cc6f68438cc5a37b77268e432b3
LiLi <aiki.nogard@gmail.com>
2017-08-20 14:26:00 -0700

    add city in weather format

--- tasklib/get_weather.py
+++ tasklib/get_weather.py
@@ -15,18 +15,21 @@ def format_weather(info):
                 'text': 'Mostly Sunny',\
                 'code': '34',\
                 'temp_c': '22',\
-                'temp': '73'}
+                'temp': '73',\
+                'city': 'shanghai'}
     >>> format_weather(info)
-    'Sun, 20 Aug 2017 11:00 AM PDT\\n22C, Mostly Sunny'
+    'SHANGHAI\\nSun, 20 Aug 2017 11:00 AM PDT\\n22C, Mostly Sunny'
     """
-    line1 = info['date']
-    line2 = str(info['temp_c']) + 'C, ' + info['text']
-    return '\n'.join([line1, line2])
+    lines = [info['city'].upper()]
+    lines.append(info['date'])
+    lines.append(str(info['temp_c']) + 'C, ' + info['text'])
+    return '\n'.join(lines)
 
 def get_weather(location):
     weather = Weather()
     info = weather.lookup_by_location(location).condition()
     info['temp_c'] = FtoC(info['temp'])
+    info['city'] = location
     return format_weather(info)
 
 if __name__ == '__main__':
